fc4a289a2c9f2faff0406ef76f9010f2aa115db9
issa AbuAwada <67484019+issa554@users.noreply.github.com>
2024-03-03 22:03:23 +0200

    Update Xreturn.py

--- Pages/Xreturn.py
+++ Pages/Xreturn.py
@@ -41,7 +41,7 @@ class Return(ck.CTkFrame):
 
         self.search_entry = ck.CTkEntry(frame,placeholder_text="search")
         self.search_entry.pack(pady=35)
-        # self.search_entry.bind("<KeyRelease>", self.search)
+        self.search_entry.bind("<KeyRelease>", self.search)
         self.table2.pack(expand=False,padx=0,pady=0)
 
         add_frame = ck.CTkFrame(frame,fg_color="transparent")
@@ -65,7 +65,7 @@ class Return(ck.CTkFrame):
         for row in self.table2.get_children():
             self.table2.delete(row)
 
-        mycursor.execute("SELECT OrderId,CustomerID,remainAmount,TotalAmount FROM Orders ORDER BY OrderDate DESC ")
+        mycursor.execute("SELECT OrderId,CustomerID,remainAmount,TotalAmount FROM Orders ORDER BY OrderId  DESC ")
         mysite = mycursor.fetchall()
         for site in mysite:
             mycursor.execute("SELECT CustomerName FROM customers where CustomerID=%s",(site[1],))
@@ -118,12 +118,14 @@ class Return(ck.CTkFrame):
         selected_item = self.table2.focus()
 
         values =  self.table2.item(selected_item, 'values')
-        mycursor.execute("SELECT ProductID , Quantity FROM orderdetails where OrderID=%s",(values[3],))
+        self.valuesO =  self.table2.item(selected_item, 'values')
+
+        mycursor.execute("SELECT ProductID , Quantity ,Subtotal FROM orderdetails where OrderID=%s",(values[3],))
         prs = mycursor.fetchall()
         for pr in prs :
-            mycursor.execute("SELECT ProductName , sell_Price FROM products where ProductID=%s",(pr[0],))
+            mycursor.execute("SELECT ProductName  FROM products where ProductID=%s",(pr[0],))
             pr_info = mycursor.fetchone()
-            self.table.insert('','end',values=(pr_info[0],pr[1],pr_info[1],pr[0]))
+            self.table.insert('','end',values=(pr_info[0],pr[1],(float(pr[2])/float(pr[1])),pr[0]))
 
         self.num_pro = ck.CTkEntry(self.de_win)
         self.num_pro.pack(pady=10)
@@ -133,8 +135,7 @@ class Return(ck.CTkFrame):
         self.re_button.pack(pady=10)
 
     def re(self):
-            selected_it = self.table2.focus()
-            valuesO =  self.table2.item(selected_it, 'values')
+
             selected_item = self.table.focus()
             if selected_item:
                 values =  self.table.item(selected_item, 'values')
@@ -147,7 +148,7 @@ class Return(ck.CTkFrame):
                         messagebox.showwarning("Warning Message",f"لا يمكنك ارجاع اكثر من {values[1]}",icon="warning")
                         return
                     update_query = "UPDATE orderdetails SET Quantity=%s WHERE OrderID=%s and ProductID=%s"
-                    update_values = (int(values[1])-int(self.num_pro.get()) , valuesO[3],values[3])
+                    update_values = (int(values[1])-int(self.num_pro.get()) , self.valuesO[3],values[3])
                     mycursor.execute(update_query, update_values)
                     mydb.commit()
 
@@ -158,40 +159,39 @@ class Return(ck.CTkFrame):
 
                     if values[1] ==0 :
                         update_query = "DELETE FROM orderdetails WHERE OrderID = %s AND ProductID = %s;"
-                        update_values = ( valuesO[3],values[3])
+                        update_values = ( self.valuesO[3],values[3])
                         mycursor.execute(update_query, update_values)
                         mydb.commit()
                         self.table.delete(selected_item)
 
 
-                    mycursor.execute("SELECT TotalAmount , remainAmount FROM orders where OrderID=%s",(valuesO[3],))
+                    mycursor.execute("SELECT TotalAmount , remainAmount FROM orders where OrderID=%s",(self.valuesO[3],))
                     ord_info = mycursor.fetchone()
+                    re_value=(float(ord_info[1]) - (int(self.num_pro.get())*float(values[2])))
+                    if re_value < 0 :
+                        insert_pay_query = "INSERT INTO payments (paymentDate,Amount,OrderID) VALUES (%s, %s,%s)"
+                        order_data = (datetime.now(), re_value,self.valuesO[3])  
+                        mycursor.execute(insert_pay_query, order_data)
+                        mydb.commit() 
+                        self.re_label = ck.CTkLabel(self.de_win,text=f"ارجاع {re_value}", font=ck.CTkFont(family="DecoType Naskh",size=35, weight="bold"))
+                                    
+                        self.re_label.pack(pady=10)
 
                     if ((float(ord_info[0]) <= (int(self.num_pro.get())*float(values[2])))):
                         update_query = "DELETE FROM orders WHERE OrderID=%s"
-                        update_values = ( valuesO[3],)
+                        update_values = ( self.valuesO[3],)
                         mycursor.execute(update_query, update_values)
                         mydb.commit()
                     else:
                         if ((float(ord_info[1]) <= (int(self.num_pro.get())*float(values[2])))):
                             update_query = "UPDATE orders SET TotalAmount=%s , remainAmount=0,receive =1  WHERE OrderID=%s"
-                            update_values = ((float(ord_info[0])-(int(self.num_pro.get())*float(values[2]))), valuesO[3])
+                            update_values = ((float(ord_info[0])-(int(self.num_pro.get())*float(values[2]))), self.valuesO[3])
                             mycursor.execute(update_query, update_values)
                             mydb.commit()
-                            if ((float(ord_info[1]) < (int(self.num_pro.get())*float(values[2])))):
-                                re_value=(float(ord_info[1]) - (int(self.num_pro.get())*float(values[2])))
-                                insert_pay_query = "INSERT INTO payments (paymentDate,Amount,OrderID) VALUES (%s, %s,%s)"
-                                order_data = (datetime.now(), re_value,valuesO[3])  
-                                mycursor.execute(insert_pay_query, order_data)
-                                mydb.commit() 
-                                self.re_label = ck.CTkLabel(self.de_win,text="ارجاع {re_value}", font=ck.CTkFont(family="DecoType Naskh",size=35, weight="bold"))
-                                
-                                self.re_label.pack(pady=10)
-
 
                         else:                           
                             update_query = "UPDATE orders SET TotalAmount=%s , remainAmount=%s WHERE OrderID=%s"
-                            update_values = ((float(ord_info[0])-(int(self.num_pro.get())*float(values[2]))),(float(ord_info[1])-(int(self.num_pro.get())*float(values[2]))), valuesO[3])
+                            update_values = ((float(ord_info[0])-(int(self.num_pro.get())*float(values[2]))),(float(ord_info[1])-(int(self.num_pro.get())*float(values[2]))), self.valuesO[3])
                             mycursor.execute(update_query, update_values)
                             mydb.commit()
 
@@ -205,6 +205,7 @@ class Return(ck.CTkFrame):
                     self.intTable()
 
 
+
     def on_item_select2(self,event):
         selected_item = self.table.focus()
         if selected_item:
@@ -212,3 +213,20 @@ class Return(ck.CTkFrame):
         else :
             self.re_button.configure(state="disabled")
   
+    def search(self, event):
+        search_query = self.search_entry.get().lower()
+
+        self.table2.delete(*self.table2.get_children())
+
+        if search_query:
+            sql = f"SELECT ProductID,ProductName,sell_Price,Unit  FROM Products WHERE LOWER(ProductName) LIKE '%{search_query}%'"
+            mycursor.execute(sql)
+            matching_products = mycursor.fetchall()
+
+            for product in matching_products:
+                self.table2.insert("", "end", values=product)
+        
+        else:self.intTable2()
+
+
+        
